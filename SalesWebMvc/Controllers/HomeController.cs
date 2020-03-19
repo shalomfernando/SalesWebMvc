220eb174eed94f1af39a5b9cb56ccd46573bad5a
shalomfernando <shalonando@gmail.com>
2020-03-19 01:43:18 -0300

    First  =c[

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales web web mvc app from C# couruse";
+            ViewData["Professor"] = "Shalom Fernando";
+
 
             return View();
         }
